804c3b4d32d13fd043b9d33cbbd8b1f8c5d7d13b
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2025-12-12 17:32:35 +0900

    compiler: define main function and base case

--- src/Language/Sparcl/CompileHaskell.hs
+++ src/Language/Sparcl/CompileHaskell.hs
@@ -4,9 +4,14 @@ import qualified Language.Sparcl.Core.Syntax as Core
 import qualified Language.Sparcl.Name as Name
 import qualified Language.Sparcl.Literal as Literal
 
---| The type for the code output
+-- The type for the code output
 data HaskellCode = HaskellCode
     { hsForward :: String
     , hsBackward :: String
     }
     deriving (Show, Eq)
+
+-- The Main Compiler Function
+compileExpression :: Core.Exp Name.Name -> HaskellCode
+compileExpression expr = case expr of
+    _ -> error "compileExpression: Unimplemented constructor"
