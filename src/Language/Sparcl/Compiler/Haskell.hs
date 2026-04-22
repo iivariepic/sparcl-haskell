040518a1d22778a226aefbefe066de5013108b67
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2026-04-23 00:32:50 +0900

    Compile unidirectional constructors and case expressions

--- src/Language/Sparcl/Compiler/Haskell.hs
+++ src/Language/Sparcl/Compiler/Haskell.hs
@@ -4,6 +4,7 @@ import qualified Language.Sparcl.Core.Syntax as Core
 import qualified Language.Sparcl.Name as Name
 import qualified Language.Sparcl.Literal as Literal
 import           Language.Sparcl.Pretty (prettyShow)
+import           Data.List
 
 -- Literals
 compileLiteral :: Literal.Literal -> String
@@ -18,6 +19,27 @@ compileBinding (name, expr) =
       nameStr = prettyShow name
   in nameStr ++ " = " ++ code
 
+-- Patterns
+compilePattern :: Core.Pat Name.Name -> String
+compilePattern pat = case pat of
+    Core.PVar n -> prettyShow n
+    Core.PCon c ps ->
+        let cName = translateConName (prettyShow c)
+            psCode = map compilePattern ps
+        in  if null psCode
+            then cName
+            else "(" ++ cName ++ " " ++ unwords psCode ++ ")"
+
+-- Helper function to translate internal constructor names
+translateConName :: String -> String
+translateConName name
+
+    | Just rest <- stripPrefix "<Tup " name =
+        let n = read (init rest) :: Int
+        in "(" ++ replicate (n - 1) ',' ++ ")"
+
+    | otherwise = name
+
 -- The Main Compiler Function
 compileExpression :: Core.Exp Name.Name -> String
 compileExpression expr = case expr of
@@ -27,11 +49,27 @@ compileExpression expr = case expr of
     -- Variables
     Core.Var n -> prettyShow n
 
-    -- Lambda Abstractions
+    -- Lambda abstractions
     Core.Abs n e ->
         let varName  = prettyShow n
             bodyCode = compileExpression e
         in "(\\" ++ varName ++ " -> " ++ bodyCode ++ ")"
+
+    -- Data constructors
+    Core.Con c es ->
+        let cName = translateConName (prettyShow c)
+            args = map compileExpression es
+        in if null args
+            then cName
+            else "(" ++ cName ++ " " ++ unwords args ++ ")"
+
+    -- Case expressions
+    Core.Case e alts ->
+        let scrutinee = compileExpression e
+            compileAlt (p, body) =
+                "  " ++ compilePattern p ++ " -> " ++ compileExpression body
+            altsCode = map compileAlt alts
+        in "(case " ++ scrutinee ++ " of {\n" ++ unlines altsCode ++ "})"
 
     -- Recursive compilation of the entire App
     Core.App e1 e2 ->
