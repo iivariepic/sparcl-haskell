c7a94b2d76be9fbf350bf86ec445e3ad844592f2
Iivari Anttila <157590564+iivariepic@users.noreply.github.com>
2026-07-29 00:29:24 +0900

    compiler: improve pretty printing

--- src/Language/Sparcl/Compiler/Haskell2.hs
+++ src/Language/Sparcl/Compiler/Haskell2.hs
@@ -40,6 +40,34 @@ data HsDecl
     | HData String [String] [(String, [String])]
     deriving (Eq, Show)
 
+-- | Strip the module prefix for checking operator/tuple names
+stripBase :: String -> String
+stripBase s | "Base." `isPrefixOf` s = drop 5 s
+            | otherwise              = s
+
+-- | Check if a string represents an operator
+isOperatorName :: String -> Bool
+isOperatorName s =
+    let s' = stripBase s
+    in not (null s') && all (`elem` "!#$%&*+./<=>?@\\^|-~:") s'
+
+-- | Check if a string represents a tuple constructor
+isTupleName :: String -> Bool
+isTupleName name =
+    let name' = stripBase name
+    in "<Tup " `isPrefixOf` name' ||
+       case stripPrefix "(" name' of
+         Just rest ->
+           case unsnoc rest of
+             Just (middle, ')') -> all (== ',') middle
+             _                  -> False
+         _ -> False
+
+-- | Helper for precedence-based parenthesization
+parensIf :: Bool -> String -> String
+parensIf True  s = "(" ++ s ++ ")"
+parensIf False s = s
+
 -- | Pretty print target patterns
 prettyHsPat :: HsPat -> String
 prettyHsPat pat = case pat of
@@ -52,24 +80,24 @@ prettyHsPat pat = case pat of
     HPBang p      -> "!" ++ prettyHsPat p
 
 -- | Pretty print target expressions
-prettyHsExpr :: HsExpr -> String
-prettyHsExpr expr = case expr of
+prettyHsExpr :: Int -> HsExpr -> String
+prettyHsExpr p expr = case expr of
     HVar v        -> v
     HCon c        -> c
     HLit l        -> l
-    HApp e1 e2    -> "(" ++ prettyHsExpr e1 ++ " " ++ prettyHsExpr e2 ++ ")"
-    HOp op e1 e2  -> "(" ++ prettyHsExpr e1 ++ " " ++ op ++ " " ++ prettyHsExpr e2 ++ ")"
-    HLam ps e     -> "(\\" ++ unwords (map prettyHsPat ps) ++ " -> " ++ prettyHsExpr e ++ ")"
-    HLet binds e  -> "(let { " ++ intercalate "; " [ prettyHsPat p ++ " = " ++ prettyHsExpr b | (p, b) <- binds ] ++ " } in " ++ prettyHsExpr e ++ ")"
-    HCase e alts  -> "(case " ++ prettyHsExpr e ++ " of {\n" ++ intercalate ";\n" [ "  " ++ prettyHsPat p ++ " -> " ++ prettyHsExpr body | (p, body) <- alts ] ++ "\n})"
-    HTuple es     -> "(" ++ intercalate ", " (map prettyHsExpr es) ++ ")"
-    HIf c t f     -> "if " ++ prettyHsExpr c ++ " then " ++ prettyHsExpr t ++ " else " ++ prettyHsExpr f
-    HError msg    -> "error " ++ show msg
+    HApp e1 e2    -> parensIf (p > 10) $ prettyHsExpr 10 e1 ++ " " ++ prettyHsExpr 11 e2
+    HOp op e1 e2  -> parensIf (p > 5)  $ prettyHsExpr 5 e1 ++ " " ++ op ++ " " ++ prettyHsExpr 6 e2
+    HLam ps e     -> parensIf (p > 0)  $ "\\" ++ unwords (map prettyHsPat ps) ++ " -> " ++ prettyHsExpr 0 e
+    HLet binds e  -> parensIf (p > 0)  $ "let { " ++ intercalate "; " [ prettyHsPat pat ++ " = " ++ prettyHsExpr 0 b | (pat, b) <- binds ] ++ " } in " ++ prettyHsExpr 0 e
+    HCase e alts  -> parensIf (p > 0)  $ "case " ++ prettyHsExpr 0 e ++ " of {\n" ++ intercalate ";\n" [ "  " ++ prettyHsPat pat ++ " -> " ++ prettyHsExpr 0 body | (pat, body) <- alts ] ++ "\n}"
+    HTuple es     -> "(" ++ intercalate ", " (map (prettyHsExpr 0) es) ++ ")"
+    HIf c t f     -> parensIf (p > 0)  $ "if " ++ prettyHsExpr 0 c ++ " then " ++ prettyHsExpr 0 t ++ " else " ++ prettyHsExpr 0 f
+    HError msg    -> parensIf (p > 10) $ "error " ++ show msg
 
 -- | Pretty print target top-level declarations
 prettyHsDecl :: HsDecl -> String
 prettyHsDecl decl = case decl of
-    HBind name e -> name ++ " = " ++ prettyHsExpr e
+    HBind name e -> name ++ " = " ++ prettyHsExpr 0 e
     HData dName tyVars cons ->
         let lhs = unwords (dName : tyVars)
             rhs = intercalate " | " [ unwords (c : args) | (c, args) <- cons ]
@@ -99,23 +127,37 @@ data BindingKind
     deriving (Eq, Show)
 
 -- | The Backward Pass structure
-data BwdResult =
-    BwdResult
-        { bwdExpr :: HsExpr
-        , bwdEnv  :: [Variable]
-        }
+data BwdResult = BwdResult
+    { bwdExpr :: HsExpr
+    , bwdEnv  :: [Variable]
+    }
 
 -- | The Compile Result Representation
-data CompileResult =
-    CompileResult
-        { forward  :: Maybe HsExpr
-        , backward :: Maybe BwdResult
-        }
+data CompileResult = CompileResult
+    { forward  :: HsExpr
+    , backward :: Maybe BwdResult
+    }
 
--- | Helper to grab just the forward expression, no matter the compilation type
+-- | Smart constructor for forward-only computations
+fwdOnly :: HsExpr -> CompileResult
+fwdOnly e = CompileResult { forward = e, backward = Nothing }
+
+-- | Smart constructor for reversible computations
+reversible :: HsExpr -> HsExpr -> [Variable] -> CompileResult
+reversible fwd bwd recs = CompileResult
+    { forward  = fwd
+    , backward = Just (BwdResult { bwdExpr = bwd, bwdEnv = recs })
+    }
+
+-- | Helper to grab just the forward expression
 getFwd :: CompileResult -> HsExpr
-getFwd (ForwardOnly e)  = e
-getFwd (Reversible f _) = f
+getFwd = forward
+
+-- | Helper to grab the backward expression
+getBwd :: CompileResult -> HsExpr
+getBwd res = case backward res of
+    Just b  -> bwdExpr b
+    Nothing -> HError "No backward expression available"
 
 -- | Helper to look up a variable's binding kind in the environment
 lookupVar :: Name.Name -> Env -> Maybe BindingKind
@@ -173,15 +215,11 @@ compileBinding ctx (name, ty, expr) =
         compiled = compileExpr initBodyCtx expr
 
     in case bKind of
-        Linear -> case compiled of
-            Reversible fwdEx (Bwd bwdEx _) ->
-                [ HBind nameStr (HTuple [HVar (nameStr ++ "_fwd"), HVar (nameStr ++ "_bwd")])
-                , HBind (nameStr ++ "_fwd") fwdEx
-                , HBind (nameStr ++ "_bwd") bwdEx
-                ]
-            ForwardOnly _ ->
-                [ HBind nameStr (HError "Compiler bug: Expected reversible computation, got ForwardOnly") ]
-
+        Linear ->
+            [ HBind nameStr (HTuple [HVar (nameStr ++ "_fwd"), HVar (nameStr ++ "_bwd")])
+            , HBind (nameStr ++ "_fwd") (getFwd compiled)
+            , HBind (nameStr ++ "_bwd") (getBwd compiled)
+            ]
         Copy ->
             [ HBind nameStr (getFwd compiled) ]
 
@@ -190,9 +228,11 @@ compilePat :: Core.Pat Name.Name -> HsPat
 compilePat pat = case pat of
     Core.PVar n -> HPVar (formatName (prettyShow n))
     Core.PCon c ps ->
-        let cName = translateConName (prettyShow c)
-            psAst = map compilePat ps
-        in HPCon cName psAst
+        let rawCName = prettyShow c
+            psAst    = map compilePat ps
+        in if isTupleName rawCName
+            then HPTuple psAst
+            else HPCon (translateConName rawCName) psAst
 
 -- | Helper for literals
 compileLiteral :: Literal.Literal -> HsExpr
@@ -200,28 +240,62 @@ compileLiteral (Literal.LitInt i) = HLit (show i)
 compileLiteral _ = HError "Unhandled literal type"
 
 -- | The expression compilation pass
-compileExpr :: CompileContext -> Core.Exp Name.Name -> Compiled
+compileExpr :: CompileContext -> Core.Exp Name.Name -> CompileResult
 compileExpr ctx expr = case expr of
 
     Core.Lit l ->
-        ForwardOnly (compileLiteral l)
+        fwdOnly (compileLiteral l)
 
     Core.Lift e1 e2 ->
-        -- Lift explicitly pairs a forward expression and a backward expression.
-        -- By definition, both sides are evaluated for their forward pass representations.
         let fwdEx = getFwd (compileExpr ctx e1)
             bwdEx = getFwd (compileExpr ctx e2)
-        in Reversible
-            { cFwd = fwdEx
-            , cBwd = Bwd { bCode = bwdEx, bReconstructs = [] }
-            }
+        in reversible fwdEx bwdEx []
 
     Core.Unlift e ->
-        -- Consumes a reversible value, extracting its forward computation
-        ForwardOnly (getFwd (compileExpr ctx e))
+        fwdOnly (getFwd (compileExpr ctx e))
 
-    -- TODO: Add Var, App, Abs, Con, RCon, Let, Case, RCase, RPin
-    _ -> ForwardOnly (HError "Not implemented")
+    Core.Var n ->
+        fwdOnly (HVar (formatName (prettyShow n)))
+
+    Core.Abs n body ->
+        let varObj  = Variable n Copy
+            bodyCtx = ctx { ctxEnv = varObj : ctxEnv ctx }
+            compiledBody = compileExpr bodyCtx body
+            pat = HPVar (formatName (prettyShow n))
+        in fwdOnly (HLam [pat] (getFwd compiledBody))
+
+    Core.App (Core.App (Core.Var op) lhs) rhs
+            | let opStr = stripBase (prettyShow op), isOperatorName opStr ->
+                let l = getFwd (compileExpr ctx lhs)
+                    r = getFwd (compileExpr ctx rhs)
+                in fwdOnly (HOp opStr l r)
+
+    -- General Application
+    Core.App e1 e2 ->
+        let res1 = compileExpr ctx e1
+            res2 = compileExpr ctx e2
+        in fwdOnly (HApp (getFwd res1) (getFwd res2))
+
+    -- Constructor Applications
+    Core.Con c es ->
+        let compiledArgs = map (getFwd . compileExpr ctx) es
+            rawCName     = prettyShow c
+        in fwdOnly $
+            if isTupleName rawCName
+                then HTuple compiledArgs
+                else foldl HApp (HCon (translateConName rawCName)) compiledArgs
+
+    Core.Case e alts ->
+        let compiledScrut = getFwd (compileExpr ctx e)
+            compileAlt (pat, body) =
+                let boundVars = map (\v -> Variable v Copy) (patVars pat)
+                    altCtx    = ctx { ctxEnv = boundVars ++ ctxEnv ctx }
+                    bodyRes   = compileExpr altCtx body
+                in (compilePat pat, getFwd bodyRes)
+            compiledAlts = map compileAlt alts
+        in fwdOnly (HCase compiledScrut compiledAlts)
+
+    _ -> fwdOnly (HError "Not implemented")
 
 
 -- | Function to compile data declarations
